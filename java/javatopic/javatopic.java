028da5180fb875c06a33bfb38b249ad5c0656588
akash patel <skypatel857@gmail.com>
2023-02-14 22:36:46 +0000

    first commit

---
+++ java/javatopic/javatopic.java
@@ -0,0 +1,9 @@
+package javatopic;
+
+public class javatopic {
+    public static void main(String[] args) {
+        System.out.println("what is java");
+        System.out.println("features of java");
+
+    }
+}
---
+++ java/mannualtopic/mannualtopic.java
@@ -0,0 +1,7 @@
+package mannualtopic;
+
+public class mannualtopic {
+    public static void main(String[] args) {
+        System.out.println("what is mannual teasting");
+    }
+}
